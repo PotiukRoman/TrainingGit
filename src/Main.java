f5927cd8af88768abf0264d717d642ea57f69a06
TeamCodeCreator <87583390+TeamCodeCreator@users.noreply.github.com>
2021-10-07 00:45:10 +0300

    Revert "delete hello from peter message"
    This reverts commit 46e81aea6271fd77c83b6ea804091984d3cf1d43.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Peter");
+        System.out.println("Hello Peter");
         System.out.println("Hello Sofia");
 
 
