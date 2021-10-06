079373a01908baf5a24f8a255705cb87395c7f8b
TeamCodeCreator <87583390+TeamCodeCreator@users.noreply.github.com>
2021-10-06 21:00:53 +0300

    add Hello Sofia message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Peter");
+        System.out.println("Hello Sofia");
+
 
         System.out.println("Hello Nick");
         System.out.println("Some other  text_01");
