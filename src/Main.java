eed29ff56415ef51b31d3adc6dc6b7fed09a85b2
TeamCodeCreator <87583390+TeamCodeCreator@users.noreply.github.com>
2021-10-06 13:21:21 +0300

    edit hello message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello Nick");
         System.out.println("Some other  text_01");
         System.out.println("Very important message");
     }
