516bc27e29b10d9056ca7faee3b6f40ed3dcd540
TeamCodeCreator <87583390+TeamCodeCreator@users.noreply.github.com>
2021-10-06 13:14:37 +0300

    edit some other text

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Some other text");
+        System.out.println("Some other  text_01");
         System.out.println("Very important message");
     }
 }
