2042b3fd3d7f50821903a91efb3c57b46175d25d
TeamCodeCreator <87583390+TeamCodeCreator@users.noreply.github.com>
2021-10-06 13:27:40 +0300

    edit hello nessage to Peter

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello Peter");
+
         System.out.println("Hello Nick");
         System.out.println("Some other  text_01");
         System.out.println("Very important message");
