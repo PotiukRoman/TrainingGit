cd0db457d03c30402e9ae64f298383d09a9a4300
TeamCodeCreator <87583390+TeamCodeCreator@users.noreply.github.com>
2021-10-06 21:04:01 +0300

    delete hello from peter message

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Nick");
-        System.out.println("Some other  text_01");
-        System.out.println("Very important message");
+        System.out.println("Hello Peter");
+        System.out.println("Hello Sofia");
+
+
     }
 }
